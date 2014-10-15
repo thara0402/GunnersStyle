b13181d5ce5eddaa0eefd04f7431cd0d91072cc1
thara0402 <thara0402@gmail.com>
2014-10-15 16:35:21 +0900

    PCLのTest

--- PCLTest/Class1.cs
+++ PCLTest/Class1.cs
@@ -2,9 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace GunnersStyle.Instapaper
+namespace PCLTest
 {
     public class Class1
     {
